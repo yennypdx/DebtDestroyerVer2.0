c4e9584ebeabab658fba250a0b15998dceb5b898
ypurwantopdx <yenny.purwanto@oit.edu>
2018-05-30 10:57:20 -0700

    Modified the CustomerDataService and its interface

--- DebtDestroyer.DataAccess/ICustomerDataService.cs
+++ DebtDestroyer.DataAccess/ICustomerDataService.cs
@@ -5,13 +5,13 @@ namespace DebtDestroyer.DataAccess
 {
     public interface ICustomerDataService
     {
-        //ICustomer GetCustomerById(int customerID);
+        ICustomer GetCustomerById(int customerID);
         void AddNewCustomer(Customer newCustomer);
         void DeleteExistingCustomer(int customerId);
         void EditCustomer(Customer customer);
         ICollection<Account> GetAccounts(int customerId);
         void GetAllocatedFunds(Customer customer);
-        int GetCustomerId(string customerName);
+        int GetCustomerByName(string customerName);
         void InsertCustomer(Customer customer);
         void SaveToStorage(System.Collections.Generic.IList<Customer> customer);
         void UpdateCustomer(Customer customer);
